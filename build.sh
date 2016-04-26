98c2203d20d7d8322024e4f39d8db0239270fb44
Muhammad Fahad Baig <muhammad.fahad.baig@outlook.com>
2016-04-26 14:23:09 +0000

    Removing unnecessary files

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./build_chmU01.sh 1
-echo "#define vdso_offset_sigtramp 0x04e0" > include/generated/vdso-offsets.h
-./build_chmU01.sh 16
--- build_chmU01.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-LOCAL_DIR=`pwd`
-export PATH=$PATH:$LOCAL_DIR/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
-
-make ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- hisi_hi6210sft_defconfig
-make ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- -j$1 
